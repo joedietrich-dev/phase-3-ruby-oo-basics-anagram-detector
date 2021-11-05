561bf45a9940846198297c9e175f0aa51995ee34
joedietrich-dev <joe@joedietrich.net>
2021-11-04 21:22:08 -0400

    commit tree d76a488e5413df1d96161ae8c7c36ef4227d0211

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
-# Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+  def match(match_array)
+    match_array.select do |match_word|
+      @word.chars.sort == match_word.chars.sort
+    end
+  end
+end
